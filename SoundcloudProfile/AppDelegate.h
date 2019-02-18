8621e427c29336d6b03592cb01cc38ed8aaac927
littlequark <stepanovtmf@gmail.com>
2019-02-18 18:41:51 +0300

    Initial Commit

---
+++ SoundcloudProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundcloudProfile
+//
+//  Created by tstepanov on 18/02/2019.
+//  Copyright © 2019 persTim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoundcloudProfile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundcloudProfile
+//
+//  Created by tstepanov on 18/02/2019.
+//  Copyright © 2019 persTim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
